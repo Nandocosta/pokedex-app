1b69cd106ca6b5dbf58a82f657ba9cd35e52db85
Fernando Castro <fernandinho.aquiraz@gmail.com>
2022-03-08 13:25:23 -0300

    card

---
+++ src/components/CardPokemon/index.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { Card } from 'antd';
+
+import { HeartOutlined } from '@ant-design/icons';
+import { Link } from "react-router-dom";
+
+import Api from "../../Services"
+
+const CardPokemon = () => {
+
+    const {pokemon, setPokemon} = useState("")
+    const {pokemonName, setPokemonName} = useState("")
+    const {pokemonType, setPokemonType} = useState("")
+
+    useEffect(() => {
+        Api.get("/Pokemons")
+        .then((response) => setPokemon(response.data))
+        .catch((e) =>{
+            console.logo(e)
+        })
+    }, [])
+    
+    return (
+        <div CardPoker>
+            <Card size="small" 
+            title="Small size card" 
+            extra={<Link to = "/PokerLikes"><HeartOutlined/></Link>} 
+            style={{ width: 300 }}>
+               
+                <p>Card content</p>
+
+            </Card>
+        </div>
+    )
+}
+export default CardPokemon;
