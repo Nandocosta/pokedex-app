aa6146c2abbf4943f99a4b078982d0d6067630bc
Fernando Castro <fernandinho.aquiraz@gmail.com>
2022-03-10 13:46:42 -0300

    consumindo a api

--- src/components/CardPokemon/index.jsx
+++ src/components/CardPokemon/index.jsx
@@ -3,38 +3,68 @@ import { Card } from 'antd';
 
 import { HeartOutlined } from '@ant-design/icons';
 import { Link } from "react-router-dom";
-// import axios from "axios";
-// import { async } from "@firebase/util";
+import axios from "axios";
+
 import './index.css'
 
 
 
 const CardPokemon = () => {
-// useEffect (() => {
 
-//     const fetchPokemon = async () => {
-    
-//         const response = await axios.get(
-//             'https://pokeapi.co/api/v2/pokemon/ditto'
-//         );
-//         console.log(response);
-//     }
+    const [pokemon, setPokemon] = useState('pikachu')
+    const [pokemonData, setPokemonData] = useState([])
+    const [pokemonType, setPokemonType] = useState('')
 
-// }, [])
-    const [buscar, setBusca] = useState()
+    const getPokemon = async () => {
+        const toArray = []
+        try{
+            const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+            const res = await axios.get(url);
+            toArray.push(res.data)
+            setPokemonType(res.data.types[0].type.name)
+            setPokemonData(toArray)
+            console.log(res)
+        }catch(e){
+            console.log(e);
+        }
+    }
+    const handleChange = (e) => {
+        setPokemon(e.target.value.toLowerCase())
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        getPokemon();
+    }
 
     return (
 
         <div >
+            <div>
+                <input type="text" onChange={handleChange}/>
+                <button onClick = {handleSubmit} >Buscar</button>
+            </div>
             <Card className ='card-poke' size="small" 
             title="Small size card" 
             extra={<Link to = "/PokerLikes"><HeartOutlined/></Link>} 
             style={{background: 'pink'}}>
-                <div>
-                    {buscar}
-                </div>
+                {pokemonData.map((data) => {
+                    return (
+                        <div>
+                            <img style={{width: '20px'}} src={data.sprites['front_default']}/>
+                            <div>
+                                <h1>Tipo</h1>
+                                {pokemonType}
+                            </div>
+                            <div>
+                                <h1>Height</h1>
+                                {''}{Math.round(data.height * 3.9)}
+                            </div>
+
+                        </div> 
+                        )
+                })}
                 
-                <button onClick={() => setBusca()}>pokemon</button>
+                {/* <button onClick={() => setPokemon(handleSubmit)}>pokemon</button> */}
             </Card>
         </div>
     )
